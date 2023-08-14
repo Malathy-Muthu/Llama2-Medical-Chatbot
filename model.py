328dacac141e3bc66dc563105f2c1dba0f7cfadb
mmuthu <malathy_muthu@intuit.com>
2023-08-14 16:56:32 -0500

    checking in the venv

--- model.py
+++ model.py
@@ -40,7 +40,8 @@ def retrieval_qa_chain(llm, prompt, db):
 def load_llm():
     # Load the locally downloaded model here
     llm = CTransformers(
-        model = "llama-2-7b-chat.ggmlv3.q8_0.bin",
+        # model = "llama-2-7b-chat.ggmlv3.q8_0.bin",
+        model = "llama-2-7b-chat-hf",
         model_type="llama",
         max_new_tokens = 512,
         temperature = 0.5
